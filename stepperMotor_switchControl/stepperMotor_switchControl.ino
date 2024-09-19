1336df2b0ccf97da5fc98b0050acb7a511878389
canusorn <39732595+canusorn@users.noreply.github.com>
2024-09-19 12:47:21 +0700

    Update stepperMotor_switchControl.ino

--- stepperMotor_switchControl/stepperMotor_switchControl.ino
+++ stepperMotor_switchControl/stepperMotor_switchControl.ino
@@ -3,6 +3,10 @@
 const int dirPin = 2; // Direction
 const int pulsePin = 3; // Pulse
 
+
+//const int dirPin = 9; // Direction
+//const int pulsePin = 11; // Pulse
+
 // กำหนดยา DIO สำหรับการอ่านค่า
 const int dioPin1 = 4; // DIO 4
 const int dioPin2 = 5; //DIO 5
@@ -11,7 +15,21 @@ const int dioPin2 = 5; //DIO 5
 const int stepsPerRevolution = 400; // จํานวน pulse ต่อการหมุน 1 รอบ
 const int pulseInterval = 2500; // หน่วงเวลาในแต่ละ pulse (2,500 ไมโครวินาที)
 
+unsigned long previousMillis = 0;
+
 void setup() {
+
+//  pinMode(8 , OUTPUT);
+//  pinMode(10, OUTPUT);
+//  pinMode(12 , OUTPUT);
+//  pinMode(13, OUTPUT);
+//  digitalWrite(8, LOW);
+//  digitalWrite(10, LOW);
+//  digitalWrite(12, LOW);
+//  digitalWrite(13, LOW);
+
+
+
   pinMode(dirPin, OUTPUT);
   pinMode(pulsePin, OUTPUT);
   pinMode(dioPin1, INPUT_PULLUP);
@@ -24,31 +42,31 @@ void loop() {
   int dioValuel = digitalRead(dioPin1); // อ่านจาก DIO 4
   int dioValue2 = digitalRead(dioPin2); // อ่านค่าจาก DIO 5
 
-  Serial.print("DIO 4 Value: ");
-  Serial.println(dioValuel);
-  Serial.print("DIO 5 Value: ");
-  Serial.println(dioValue2);
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= 1000) {
+    previousMillis = currentMillis;
+    if (dioValuel == LOW) Serial.println("Right Dir");
+    else if (dioValue2 == LOW) Serial.println("Left Dir");
+    else Serial.println("Stop");
+  }
 
   if (dioValuel == LOW) {
     digitalWrite(dirPin, HIGH); //หมุนไปทางขวา
 
-    for (int i = 0; i < stepsPerRevolution; i++) {
-      digitalWrite(pulsePin, HIGH);
-      delayMicroseconds(pulseInterval / 2);
-      digitalWrite(pulsePin, LOW);
-      delayMicroseconds(pulseInterval / 2);
-    }
-  }
-  else if (dioValue2 == LOW) {
+    digitalWrite(pulsePin, HIGH);
+    delayMicroseconds(pulseInterval / 2);
+    digitalWrite(pulsePin, LOW);
+    delayMicroseconds(pulseInterval / 2);
+
+  }  else if (dioValue2 == LOW) {
     digitalWrite(dirPin, LOW); // หมุนไปทางซ้าย
 
-    for (int i ; i < stepsPerRevolution; i++) {
-      digitalWrite(pulsePin, HIGH);
-      delayMicroseconds(pulseInterval / 2);
-      digitalWrite(pulsePin, LOW);
-      delayMicroseconds(pulseInterval / 2);
-    }
+    digitalWrite(pulsePin, HIGH);
+    delayMicroseconds(pulseInterval / 2);
+    digitalWrite(pulsePin, LOW);
+    delayMicroseconds(pulseInterval / 2);
   } else {
     delay(1);
   }
+
 }
